8078f5e7143a0564c8952787c394b9415c029297
Ashley <pinguinlars1105@gmail.com>
2026-04-02 18:56:07 +0200

    refactor: use shared sprite batch

--- core/src/main/kotlin/me/ashypinguin/atvsrg/AtVsrGame.kt
+++ core/src/main/kotlin/me/ashypinguin/atvsrg/AtVsrGame.kt
@@ -3,14 +3,26 @@ package me.ashypinguin.atvsrg
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxGame
-import ktx.app.KtxScreen
 import ktx.async.KtxAsync
+import me.ashypinguin.atvsrg.screens.AbstractScreen
 import me.ashypinguin.atvsrg.screens.MainMenuScreen
 
-class AtVsrGame : KtxGame<KtxScreen>() {
+/**
+ * The main game instance.
+ *
+ * DO NOT CALL MANUALLY
+ */
+class AtVsrGame : KtxGame<AbstractScreen>() {
+
+  /** Share sprite batch */
   val batch by lazy { SpriteBatch() }
+
+  /** Default arial font used by LIBgdx */
   val font by lazy { BitmapFont() }
 
+  /**
+   * Main entrypoint of the game.
+   */
   override fun create() {
     KtxAsync.initiate()
 
@@ -18,6 +30,9 @@ class AtVsrGame : KtxGame<KtxScreen>() {
     setScreen<MainMenuScreen>()
   }
 
+  /**
+   * Disposes all shared resources and calls [KtxGame.dispose]
+   */
   override fun dispose() {
     batch.dispose()
     font.dispose()
