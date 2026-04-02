eafe02823e28ea1b2061baa8029762fcfa07e536
Ashley <pinguinlars1105@gmail.com>
2026-04-02 20:12:35 +0200

    docs: add util function doc

--- core/src/main/kotlin/me/ashypinguin/atvsrg/Utils.kt
+++ core/src/main/kotlin/me/ashypinguin/atvsrg/Utils.kt
@@ -2,6 +2,13 @@ package me.ashypinguin.atvsrg
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
+/**
+ * Start a [SpriteBatch] render with the [game][AtVsrGame] instance.
+ *
+ * @receiver the main [game][AtVsrGame] instance
+ * @param scope Function block that gets called between [SpriteBatch.begin] and [SpriteBatch.end]
+ * @see ktx.graphics.use
+ */
 fun AtVsrGame.withBatch(scope: SpriteBatch.(AtVsrGame) -> Unit) {
   batch.begin()
   batch.scope(this)
