f705a1cb58e30bad941fbc363e6259a3f5852424
Lars <pinguinlars1105@gmail.com>
2026-04-02 20:28:17 +0200

    docs: refactor KDoc block tag to a better one

--- core/src/main/kotlin/me/ashypinguin/atvsrg/screens/AbstractScreen.kt
+++ core/src/main/kotlin/me/ashypinguin/atvsrg/screens/AbstractScreen.kt
@@ -7,6 +7,6 @@ import me.ashypinguin.atvsrg.AtVsrGame
  * Super class for all screens.
  * It gives access to shared recourses in [AtVsrGame]
  *
- * @param game the main game instance that can be used to access shared recourses
+ * @property game the main game instance that can be used to access shared recourses
  */
 abstract class AbstractScreen(protected val game: AtVsrGame) : KtxScreen
