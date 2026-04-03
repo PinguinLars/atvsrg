ea0217e5072691a24058ccf028ca412beba695fc
Lars <pinguinlars1105@gmail.com>
2026-04-03 16:10:45 +0200

    feat: add input handles

--- core/src/main/kotlin/me/ashypinguin/atvsrg/screens/GameScreen.kt
+++ core/src/main/kotlin/me/ashypinguin/atvsrg/screens/GameScreen.kt
@@ -1,6 +1,30 @@
 package me.ashypinguin.atvsrg.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.utils.Align
+import me.ashypinguin.atvsrg.withBatch
 import me.ashypinguin.atvsrg.AtVsrGame
+import me.ashypinguin.atvsrg.exit
 import me.ashypinguin.atvsrg.maps.BeatMap
 
-class GameScreen(game: AtVsrGame, /* Temporary */ map: BeatMap? = null) : AbstractScreen(game)
+class GameScreen(game: AtVsrGame, /* Temporary */ map: BeatMap? = null) : AbstractScreen(game) {
+  var lastPressedKey = ' '
+
+  override fun render(delta: Float) {
+    game.viewport.apply()
+    game.batch.projectionMatrix = game.viewport.camera.combined
+
+    game.withBatch {
+      it.font.draw(this,"Last key: $lastPressedKey",0f, it.viewport.worldHeight * .9f, it.viewport.worldWidth, Align.center, false)
+    }
+
+    when {
+      Gdx.input.isKeyPressed(Input.Keys.D) -> lastPressedKey = 'd'
+      Gdx.input.isKeyPressed(Input.Keys.F) -> lastPressedKey = 'f'
+      Gdx.input.isKeyPressed(Input.Keys.J) -> lastPressedKey = 'j'
+      Gdx.input.isKeyPressed(Input.Keys.K) -> lastPressedKey = 'k'
+      Gdx.input.isKeyPressed(Input.Keys.Q) -> game.exit()
+    }
+  }
+}
