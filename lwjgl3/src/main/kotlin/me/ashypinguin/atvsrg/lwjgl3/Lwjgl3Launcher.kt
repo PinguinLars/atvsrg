3ba1b9f63356098f8730d3b1f3d33b1317a9b56c
Lars <pinguinlars1105@gmail.com>
2026-04-09 19:24:01 +0200

    chore: add error handling

--- lwjgl3/src/main/kotlin/me/ashypinguin/atvsrg/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/me/ashypinguin/atvsrg/lwjgl3/Lwjgl3Launcher.kt
@@ -6,8 +6,12 @@ package me.ashypinguin.atvsrg.lwjgl3
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration.getDisplayMode
+import com.badlogic.gdx.utils.GdxRuntimeException
 import me.ashypinguin.atvsrg.Atvsrg
+import java.lang.System.err
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.system.exitProcess
 
 private const val LOG_ENV = "ATVSRG_LOG"
 
@@ -36,5 +40,24 @@ fun main() {
     setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
   }
 
-  Lwjgl3Application(Atvsrg(logLevel), config)
+  try {
+    Lwjgl3Application(Atvsrg(logLevel), config)
+  } catch (e: Throwable) {
+    if (logLevel > Application.LOG_NONE) {
+      val format = DateTimeFormatter.ofPattern("ddMMMYY HH:mm:ss.SSS")!!
+      val getDate = { LocalDateTime.now().format(format)!! }
+      val location = "me.ashypinguin.atvsrg.Lwjgl3Launcher"
+      if (e.cause != null && e is GdxRuntimeException) {
+        err.println("[FATAL] [${getDate()}] $location: A fatal error of type ${e.cause!!::class.java.name} with the reason \"${e.cause!!.message}\" has been thrown.")
+        if (logLevel >= Application.LOG_DEBUG) e.printStackTrace(err)
+        else println("[INFO] [${getDate()}] $location: Stacktraces are limited to the debug loglevel. To enable run with \"ATVSRG_LOG=debug\".")
+        err.println("[FATAL] [${getDate()}] $location: Exiting...")
+      } else {
+        // This shouldn't be reachable
+        err.println("[FATAL] [${getDate()}] $location: Something has seriously gone wrong.")
+        throw e
+      }
+    }
+    exitProcess(1)
+  }
 }
